427032f8dbd52a551a2e7afabbfa055a3b8ba38a
Devin Arena <djarena3577@eagle.fgcu.edu>
2023-05-08 12:18:48 -0400

    attack/defense for monsters

--- commands/ydkid.js
+++ commands/ydkid.js
@@ -15,7 +15,12 @@ module.exports = {
 
                 const image_url = card['card_images'][0]['image_url'];
 
-                const embed = new EmbedBuilder().setColor(0x0099FF).setTitle(card['name']).setDescription(card['desc']).setImage(image_url);
+                const embed = new EmbedBuilder().setColor(0x0099FF).setImage(image_url)
+                    .setTitle(card['name']).setDescription(card['desc']);
+
+                if ("atk" in card && "def" in card) {
+                    embed.addFields({ name: "Attack", value: card["atk"].toString(), inline: true }, { name: "Defense", value: card["def"].toString(), inline: true })
+                }
 
                 await interaction.reply({ embeds: [embed] });
             }).catch(err => {
